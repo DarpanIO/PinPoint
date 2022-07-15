e74ec12c259403aa06184641a6a56ba02bf49c20
darpan9460@gmail.com <darpan9460@gmail.com>
2022-07-15 11:34:40 +0530

    Completed Navbar

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 
 import './MainNavigation.css';
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = props => {
+    const [drawerIsOpen,setDrawerIsOpen]=useState(false);
+
+    const openDrawerHandler=()=>{
+        setDrawerIsOpen(true);
+    }
+    const closeDrawerHandler=()=>{
+        setDrawerIsOpen(false);
+    }
     return <React.Fragment>
-    <SideDrawer>
+    {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
+    <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
             <NavLinks />
         </nav>
-    </SideDrawer>
+    </SideDrawer>)
     <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
             <span />
             <span />
             <span />
