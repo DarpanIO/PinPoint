6eadd7b09b662f8ccfefd5997eb198fd5b951ce4
darpan9460@gmail.com <darpan9460@gmail.com>
2022-07-26 11:01:54 +0530

    added form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
 export default function NewPlace() {
-  return (
-    <div>NewPlace Works</div>
-  )
+  return <form className="place-form">
+  <Input element="input" type="text" label="Title" />
+  </form> 
 }
