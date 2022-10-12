63924d1151d510502a8b6a5bbdb03d748d014c0f
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-12 14:08:56 +0530

    Added maps

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -19,10 +19,10 @@ export default function PlaceItem(props) {
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="map_container" style={{padding: "5px"}}>
-The Map
-           {/* <iframe title="map" width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src={'https://maps.google.com/maps?q=' + props.coordinates.lat.toString() + ',' + props.coordinates.lng.toString() + '&t=&z=15&ie=UTF8&iwloc=&output=embed'}></iframe> */}
-           {/* <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=5a33be79e53caf0a07dfec499abf84b7b481f165'></script> */}
+        <div className="map_container" style={{ padding: "5px" }}>
+          <Map center={props.cordinates} zoom={16} />
+          {/* <iframe title="map" width="100%" height="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src={'https://maps.google.com/maps?q=' + props.coordinates.lat.toString() + ',' + props.coordinates.lng.toString() + '&t=&z=15&ie=UTF8&iwloc=&output=embed'}></iframe> */}
+          {/* <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=5a33be79e53caf0a07dfec499abf84b7b481f165'></script> */}
         </div>
       </Modal>
       <li className="place-item">
