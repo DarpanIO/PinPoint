ec645fe70d2c0f0c249ae2d68fb6090de885321f
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-20 01:33:01 +0530

    Added update place

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Users from './user/pages/users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UpdatePlace from './places/pages/UpdatePlace';
 function App() {
   return <Router>
   <MainNavigation />
@@ -13,6 +14,7 @@ function App() {
     <Route exact path="/" element={<Users />}></Route>
     <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
     <Route exact path="/places/new" element={<NewPlace />}></Route>
+    <Route exact path="/places/:placeId" element={<UpdatePlace/>}></Route>
     <Route path="/*" element={ <Navigate to="/" /> } />
   </Routes>
   </main>
