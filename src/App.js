8b47f355885299b3c3614bbe65b285ff078dd3ca
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-21 00:16:07 +0530

    Added Auth page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
 function App() {
   return <Router>
   <MainNavigation />
@@ -15,6 +16,7 @@ function App() {
     <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
     <Route exact path="/places/new" element={<NewPlace />}></Route>
     <Route exact path="/places/:placeId" element={<UpdatePlace/>}></Route>
+    <Route exact path="/auth" element={<Auth/>}></Route>
     <Route path="/*" element={ <Navigate to="/" /> } />
   </Routes>
   </main>
