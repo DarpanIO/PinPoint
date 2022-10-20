0a2f66e792158bdf588a9a74a1f93b581169dca2
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-21 00:41:29 +0530

    Fixed routes for auuthenticated and unautharized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import {BrowserRouter as Router,Route,Routes,Navigate} from 'react-router-dom';
-import Users from './user/pages/users';
-import NewPlace from './places/pages/NewPlace';
-import UserPlaces from './places/pages/UserPlaces';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UpdatePlace from './places/pages/UpdatePlace';
-import Auth from './user/pages/Auth';
-function App() {
-  return <Router>
-  <MainNavigation />
-  <main>
+import React, { useState, useCallback } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Users from "./user/pages/users";
+import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
 
-  <Routes>
-    <Route exact path="/" element={<Users />}></Route>
-    <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
-    <Route exact path="/places/new" element={<NewPlace />}></Route>
-    <Route exact path="/places/:placeId" element={<UpdatePlace/>}></Route>
-    <Route exact path="/auth" element={<Auth/>}></Route>
-    <Route path="/*" element={ <Navigate to="/" /> } />
-  </Routes>
-  </main>
-  </Router>;
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />}></Route>
+        <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
+        <Route exact path="/places/new" element={<NewPlace />}></Route>
+        <Route exact path="/places/:placeId" element={<UpdatePlace />}></Route>
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    );
+  } else {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />}></Route>
+        <Route exact path="/:userId/places" element={<UserPlaces />}></Route>
+        <Route exact path="/auth" element={<Auth />}></Route>
+        <Route path="/*" element={<Navigate to="/auth" />} />
+      </Routes>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import './NavLinks.css'
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { AuthContext } from '../../context/auth-context';
+import './NavLinks.css';
+
 const NavLinks = props => {
-    return <ul className="nav-links">
+  const auth = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/" exact>ALL USERS</NavLink>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
+      )}
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/u1/places" exact>MY PLACES</NavLink>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
         </li>
+      )}
+      {!auth.isLoggedIn && (
         <li>
-            <NavLink to="/places/new" exact>ADD PLACE</NavLink>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
         </li>
-        <li>
-            <NavLink to="/auth  " exact>AUTHENTICATE</NavLink>
-        </li>
+      )}
     </ul>
+  );
 };
 
-export default NavLinks ;
+export default NavLinks;
