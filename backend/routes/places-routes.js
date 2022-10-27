e6b9c8df880a7cddc70aa7cc646e865d831de2b6
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-27 23:59:27 +0530

    Added update places

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -16,8 +16,7 @@ router.post(
 );
 router.patch("/:pid",  [
     check("title").not().isEmpty(),
-    check("description").isLength({ min: 5 }),
-    check("address").not().isEmpty(),
+    check("description").isLength({ min: 5 })
   ], placesControllers.updatePlace);
 router.delete("/:pid", placesControllers.deletePlace);
 
