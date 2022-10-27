37d48c2bb49e9acfae37d41287c6f44b5f520179
darpan9460@gmail.com <darpan9460@gmail.com>
2022-10-28 02:01:50 +0530

    Added Update and Delete api with database

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -101,37 +101,53 @@ const createPlace = async (req, res, next) => {
   res.status(201).json(createdPlace);
 };
 
-const updatePlace = async(req, res, next) => {
+const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    const error= new HttpError("Invalid Input response,please check your data", 422);
-    return next(error)
+    const error = new HttpError(
+      "Invalid Input response,please check your data",
+      422
+    );
+    return next(error);
   }
   const { title, description } = req.body;
   const placeId = req.params.pid;
   let place;
-  try{
-    place= await Place.findById(placeId)
-  }catch(err){
-    const Error= new HttpError("Something went wrong , could not update",500)
-    return next(Error)
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const Error = new HttpError("Something went wrong , could not update", 500);
+    return next(Error);
   }
   place.title = title;
   place.description = description;
-  try{
-    await place.save()
-  }catch(err){
-    const Error= new HttpError("Something went wrong , could not update",500)
-    return next(Error)
+  try {
+    await place.save();
+  } catch (err) {
+    const Error = new HttpError("Something went wrong , could not update", 500);
+    return next(Error);
   }
-  res.status(200).json({ place: place.toObject({getters:true}) });
+  res.status(200).json({ place: place.toObject({ getters: true }) });
 };
 
-
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
-  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+let place;
+try{
+  place = await Place.findById(placeId);
+}catch(err){
+  console.log(err)
+  const Error= new HttpError("Something went wrong , could not delete 1", 500)
+  return next(Error)
+}
+try{
+  await place.remove()
+}catch(err){
+  const Error= new HttpError("Something went wrong , could not delete 2", 500)
+  return next(Error)
+}
+
   res.status(200).json({ message: "Deleted Place" });
 };
 
