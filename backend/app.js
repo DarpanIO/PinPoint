580f84be4f0846ffba285b1f54881dbb622088d9
darpan9460@gmail.com <darpan9460@gmail.com>
2022-11-06 16:53:34 +0530

    fixed cors error

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ const port = 5000;
 console.log("Server started at http://localhost:" + port);
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept,Authorization ,Origin, X-Requested-With');
+  next();
+});
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
