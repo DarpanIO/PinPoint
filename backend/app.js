7a408d656e11562f9cd0c485d2a1a464b083ee30
darpan9460@gmail.com <darpan9460@gmail.com>
2022-11-06 17:36:22 +0530

    Added Error modal frontend

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept,Authorization ,Origin, X-Requested-With');
   next();
 });
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,8 +1,9 @@
 import React, { useState, useContext } from "react";
-
 import Card from "../../shared/components/UIElements/Card";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -15,6 +16,8 @@ import "./Auth.css";
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -59,6 +62,7 @@ const Auth = () => {
     if (isLoginMode) {
     } else {
       try {
+        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -71,16 +75,28 @@ const Auth = () => {
           }),
         });
         const responseData = await response.json();
+        if(!response.ok){
+          throw new Error(responseData.message)
+        }
         console.log(responseData);
+        setIsLoading(false);
+        auth.login(); 
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong , Please Try Again');
       }
     }
-    auth.login();
   };
 
+  const errorHandler= () => {
+    setError(null)
+  }
   return (
+    <>
+    <ErrorModal error={error} onClear={errorHandler}/>
     <Card className="authentication">
+    {isLoading && <LoadingSpinner asOverlay />}
       <h2>Login Required</h2>
       <hr />
       <form onSubmit={authSubmitHandler}>
@@ -121,6 +137,7 @@ const Auth = () => {
         SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
       </Button>
     </Card>
+    </>
   );
 };
 
