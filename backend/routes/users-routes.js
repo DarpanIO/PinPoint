b046c055a624d4204d3b4d2a9211068cdd3c22de
darpan9460@gmail.com <darpan9460@gmail.com>
2022-11-14 01:16:59 +0530

    added multer configuration

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { check } = require("express-validator");
 const usersControllers = require("../controllers/users-controllers");
 const router = express.Router();
+const fileUpload=require('../middleware/file-upload')
 
 
 router.get("/", usersControllers.getUsers);
-router.post("/signup",  [
+router.post("/signup", fileUpload.single('image') ,[
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
     check("password").isLength({ min: 8 }),
